46250590e0cf3adc8610d4855e882de0687ec1cb
frensing <dev@frensing.de>
2024-02-12 18:51:54 +0200

    tetromino t

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -2,16 +2,18 @@ import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
   shape;
+  orientations;
 
-  constructor(shape) {
+  constructor(shape, orientations) {
     this.shape = shape
+    this.orientations = orientations
   }
 
-  static T_SHAPE = RotatingShape.fromString(
+  static T_SHAPE = new Tetromino(RotatingShape.fromString(
     `.T.
      TTT
      ...`
-  )
+  ), 4);
 
-  
+  toString() {return this.shape.toString()}
 }
